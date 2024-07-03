05acbd86290ab14e8aca60cdd6ebb447e768607d
josh <takalth@hotmail.com>
2024-07-02 23:20:05 -0600

    Minor fixes

--- src/pages/other-projects.js
+++ src/pages/other-projects.js
@@ -8,24 +8,26 @@ export default function OtherProjects() {
       <Head>
         <title>Other Projects</title>
       </Head>
-      <div className="topSection generalText">
+      <div className="topSection">
         <h1>Other Projects</h1>
-        <h3>
-          <Link href="https://indulgentcreativity.blogspot.com/">
-            Indulgent Creativity
-          </Link>
-        </h3>
-        <p>
-          I have a few other things going on. Notably, I'm working on building
-          an RPG in Unity, but it's still a long ways out from release.
-          Indulgent Creativity is a blog where I outline many of my thoughts on
-          game design.
-        </p>
-        <h3>Upcoming</h3>
-        <p>
-          There are a few other things cooking. I am excited to update this when
-          they are added, but for now I have to leave it here.
-        </p>
+        <div className="generalText">
+          <h3>
+            <Link href="https://indulgentcreativity.blogspot.com/">
+              Indulgent Creativity
+            </Link>
+          </h3>
+          <p>
+            I have a few other things going on. Notably, I'm working on building
+            an RPG in Unity, but it's still a long ways out from release.
+            Indulgent Creativity is a blog where I outline many of my thoughts
+            on game design.
+          </p>
+          <h3>Upcoming</h3>
+          <p>
+            There are a few other things cooking. I am excited to update this
+            when they are added, but for now I have to leave it here.
+          </p>
+        </div>
       </div>
     </>
   );
